adb637eec3bd602d91b4c59a56cd04558226bfa9
admin <admin@YD-15>
2022-07-01 18:01:30 +0900

    homework

--- HelloWorld/src/co/edu/Example08.java
+++ HelloWorld/src/co/edu/Example08.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Example08 {
 public static void main(String[] args) {
 	System.out.println("[필수 정보 입력]");
-	System.out.println("1. 이름 : ____________");
+	System.out.print("1. 이름 : ");
 	Scanner sn = new Scanner(System.in);
 	String name = sn.nextLine();
 
-	System.out.println("2. 주민번호 앞 6자리 : ____________");
+	System.out.print("2. 주민번호 앞 6자리 : ");
 	Scanner sn2 = new Scanner(System.in);
 	int num = sn2.nextInt();
 	
-	System.out.println("3. 전화번호 : ____________");
+	System.out.print("3. 전화번호 : ");
 	Scanner sn3 = new Scanner(System.in);
 	String tel = sn3.nextLine();
 	
