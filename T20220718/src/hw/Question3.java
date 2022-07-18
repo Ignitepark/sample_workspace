4d9b1772bb93f1505503d7b5c065e27b49d8acec
admin <admin@YD-15>
2022-07-18 18:15:11 +0900

    220718

---
+++ T20220718/src/hw/Question3.java
@@ -0,0 +1,16 @@
+package hw;
+
+public class Question3 {
+	public static void main(String[] args) {
+		// 문제3) 각 연산식을 대입 연산자 하나로 구성된 연산식으로 수정하세요.
+		int val = 0;
+
+		 val = 10;
+
+		 val = 5;
+
+		 val = 15;
+
+		 val = 3;
+	}
+}
